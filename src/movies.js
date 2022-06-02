f0bf54902ff411a864dec11f21ffc3179887c993
CristianMP04 <christian.marti.pernin@gmail.com>
2022-06-02 15:49:39 +0200

    End Iteration 2
    End Iteration 2

--- src/movies.js
+++ src/movies.js
@@ -15,7 +15,14 @@ function getAllDirectors(movies) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  const dramaSpilberg  = movies.filter((dramaSpil) => {
+    return dramaSpil.director === 'Steven Spielberg' && dramaSpil.genre.includes('Drama');
+  
+  })
+
+  return dramaSpilberg.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage() {}
